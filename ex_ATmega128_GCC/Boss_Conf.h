e9a7c3614a72ca18061883cffdf6576dead35cae
rtboss <rtboss@815b79aa-b098-0410-98f2-e32a66b021fa>
2012-01-04 08:41:31 +0000

    v1.20 	(2012/01/03) - 파일이름 및 구조 변경. - 메모리 할당 및 해제 속도 항상. - 메모리 Pool 초기화 함수 외부호출 방식으로 변경. _Boss_mem_pool_init() 호출후 사용. - 메모리 정보관련 _BOSS_MEM_INFO_ 추가. - Kernel의 sleep 기능을 Timer와 통합. - _Boss_start_schedule() 호출시 인자 전송방식에서 함수호출 방식으로변경 _Boss_start_tcb_sp() - Cortex-M3 SVC 처리 부분 제거. - Mail Box 전송시 FIFO방식으로 변경. - Mail Box Pand 시 메시지 처리 결과와 응답 결과값을 분리. - 세마포어 및 Mail Box Pand 시 우선순위 제어 기능 제외. - 크리티컬 섹션, 변수 저장방식 추가. - TCB 상태를 WAITING(대기) / LISTING(실행중 또는 준비) 두가지 상태로 축소. - TCB 외부 확장 기능 추가. (현재 미사용) - 타이머를 외부 선언후 사용하도록함. (자동할당 안함)
    /*---------------------------------------------------------------------------------------*/
    v1.13 	(2011/02/08)
    - _Boss_schedule()에서 처리하던 "_current_tcb->status = TASK_SUSPEND"를 
      _Boss_switch_current_tcb()에서 처리 하도록함.
    - 문맥전환시 인터럽트 중지 및 복원을 "_Boss_context_switch()"에서 MCU별로 처리하도록함.
    - ATmega 스택 구성 변경 및 SREG 추가.
    
    /*---------------------------------------------------------------------------------------*/
    v1.12 	(2011/01/14)
    - Mail Box에 우선순위 인자를 추가함.
    
    /*---------------------------------------------------------------------------------------*/
    v1.11
    - Boss_wait() 시그널 확인 IRQ 중지 후 비교.
    
    /*---------------------------------------------------------------------------------------*/
    v1.10   (2010/12/15)
    - Cortex-M3 IAR 지원
    - ATmage128 GCC 지원
    
    - 폴더 구조 및 함수 이름 변경
    - args TCB 저장방식으로 변경
    
    - Boss_Conf.c 파일 추가.(설정 함수)
    - 타이머, 메시지, 메일박스의 메모리 할당 방법 변경
      * Boss_Tmr    : _BOSS_TMR_ALLOC() / _BOSS_TMR_FREE()
      * Boss_Q_Mail : _BOSS_MAIL_Q_BOX_ALLOC() / _BOSS_MAIL_Q_BOX_FREE()
      * Boss_Q_Msg  : _BOSS_MSG_FIFO_ALLOC() / _BOSS_MSG_FIFO_FREE()
    
    - Boss_task_delete() 반환값(sp_base) 제거
    - 스케줄러 정렬시 "prio_orig" 비교 루틴제거
    - 문맥전환 실행시 "_Boss_schedule()" 호출하여 "_Boss_context_switch()" 실행하도록함.
    - 메일박스 세마포어, 스케줄러(시그널 전송) 실행시 IRQ 활성화 상태로함.
    - Cortex-M3 SVC 처리 함수 C언어로 구현(스택 확인 코드 ASM) 
    
    
    /*---------------------------------------------------------------------------------------*/
    v1.00   (2010/11/30)
    - RT-BOSS (Cortex-M3 Keil)
    - 커널 / 세마포어 / 타이머 / 메시지 / 메일박스 / 메모리 관리 기능구현
    
    git-svn-id: https://dev.naver.com/svn/rt-boss/trunk@6 815b79aa-b098-0410-98f2-e32a66b021fa

---
+++ ex_ATmega128_GCC/Boss_Conf.h
@@ -0,0 +1,144 @@
+#ifndef _BOSS_CONF_H_
+#define _BOSS_CONF_H_
+/*
+*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*
+*                                                                             *
+*                 RT-BOSS (Config File)         [ ATmega128 ]                 *
+*                                                                             *
+*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*
+*/
+
+/*===========================================================================*/
+/*                            RT-BOSS 데이터형                               */
+/*---------------------------------------------------------------------------*/
+typedef unsigned char       boss_u08_t;     /* unsigned  8bit 데이터형 */
+typedef unsigned int        boss_u16_t;     /* unsigned 16bit 데이터형 */
+typedef unsigned long int   boss_u32_t;     /* unsigned 32bit 데이터형 */
+
+//typedef boss_u08_t          boss_byte_t;      /* Byte                 */
+typedef boss_u08_t          boss_reg_t;       /* MCU 레지스터 크기      */
+typedef boss_u16_t          boss_uptr_t;      /* unsigned 포인터 크기   */
+typedef boss_u08_t          boss_stk_t;       /* 스택                   */
+
+typedef boss_u16_t          boss_sigs_t;      /* 시그널                 */
+typedef boss_u32_t          boss_tmr_ms_t;    /* 타이머 카운트(ms)      */
+
+typedef boss_u16_t          boss_mem_align_t; /* 메모리 정렬            */
+
+/*===========================================================================*/
+/*   IRQ (Interrupt request) / ISR (Interrupt Service Routine)               */
+/*---------------------------------------------------------------------------*/
+#define BOSS_IRQ_DISABLE()    do { \
+                                boss_reg_t _irq_storage_ = SREG; \
+                                asm volatile("cli")
+                                    
+#define BOSS_IRQ_RESTORE()      SREG = _irq_storage_; \
+                              } while(0)
+
+/*----------------------------------------------------------------------*/
+#define BOSS_IRQ_LOCK_SR( _sr_ )  \
+                    do { _sr_ = SREG; asm volatile("cli"); } while(0)
+            
+#define BOSS_IRQ_FREE_SR( _sr_ )      do { SREG = _sr_; } while(0)
+
+/*----------------------------------------------------------------------*/
+#define _BOSS_IRQ_()    ( (SREG & (1 << SREG_I)) ? 0 : !0 ) /* 0 = Enable / !0 = Disable */
+
+#define _BOSS_ISR_()    _mcu_isr_()    /* !0 = ISR Active  */
+
+/*----------------------------------------------------------------------*/
+#define _BOSS_ISR_BEGIN()   do { _Boss_sched_lock(); _mcu_isr_begin()
+#define _BOSS_ISR_FINIS()   _mcu_isr_finis(); _Boss_sched_free(); } while(0)
+
+boss_reg_t _mcu_isr_(void);
+void _mcu_isr_begin(void);
+void _mcu_isr_finis(void);
+
+
+/*===========================================================================*/
+/*                           RT-BOSS 사용자 설정                             */
+/*---------------------------------------------------------------------------*/
+//#define _BOSS_TCB_EXT_                /* TCB 확장(extend) */
+#define _BOSS_MEM_INFO_                 /* 메모리 디버거 정보 */
+
+#define _BOSS_TICK_MS_          10      /* Tick (ms)  */
+#define _IDLE_STACK_BYTES       128     /* Bytes      */
+#define _BOSS_MEM_POOL_SIZE     1024    /* Bytes      */
+
+
+/*===========================================================================*/
+/*                                    ASSERT                                 */
+/*---------------------------------------------------------------------------*/
+#define BOSS_ASSERT(expr)  do { if(!(expr)) _assert(PSTR(__FILE__), __LINE__); } while(0)
+void _assert(const char *file, unsigned int line);
+
+
+/*===========================================================================*/
+/*                             태스크 우선순위                               */
+/*---------------------------------------------------------------------------*/
+typedef enum {
+  PRIO_BOSS_KERNEL = 0,   /* 상위 우선순위 */
+  
+  AA_PRIO_1,              /* 1. TOP */
+  BB_PRIO_2,              /* 2 */
+  
+  PRIO_BOSS_IDLE = 255,   /* 하위 우선순위 */
+} boss_prio_t;
+
+
+/*===========================================================================*/
+/*                           메시지 큐 명령어                                */
+/*---------------------------------------------------------------------------*/
+typedef enum {
+  M_CMD_EMPTY = 0,
+
+  M_CMD_1,
+  M_CMD_2,
+  /* ... */
+  
+  M_CMD_MAX = 255,
+} msg_cmd_t;
+
+
+/*===========================================================================*/
+/*                               INCLUDE FILE                                */
+/*---------------------------------------------------------------------------*/
+#include <avr/io.h>
+
+#include <stdio.h>        // "printf 사용을 위해"
+#include <avr/pgmspace.h>
+
+#include "Boss.h"
+#include "Boss_Tmr.h"
+#include "Boss_Mem.h"
+#include "Boss_Q_Msg.h"
+#include "Boss_Q_MBox.h"
+#include "Boss_Sem.h"
+
+
+/*===========================================================================*/
+/*                     USER DEFINE & FUNCTION PROTOTYPES                     */
+/*---------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------*/
+/*                       시그널 비트 정의                          */
+
+/* SIG_15_BIT ~ SIG_12_BIT (상위 4비트는 RT-BOSS에서 사용함)      */
+#define SIG_11_BIT          (boss_sigs_t)(1 << 11)      /* 0x0800 */
+#define SIG_10_BIT          (boss_sigs_t)(1 << 10)      /* 0x0400 */
+#define SIG_09_BIT          (boss_sigs_t)(1 << 9)       /* 0x0200 */
+#define SIG_08_BIT          (boss_sigs_t)(1 << 8)       /* 0x0100 */
+
+#define SIG_07_BIT          (boss_sigs_t)(1 << 7)       /* 0x0080 */
+#define SIG_06_BIT          (boss_sigs_t)(1 << 6)       /* 0x0040 */
+#define SIG_05_BIT          (boss_sigs_t)(1 << 5)       /* 0x0020 */
+#define SIG_04_BIT          (boss_sigs_t)(1 << 4)       /* 0x0010 */
+#define SIG_03_BIT          (boss_sigs_t)(1 << 3)       /* 0x0008 */
+#define SIG_02_BIT          (boss_sigs_t)(1 << 2)       /* 0x0004 */
+#define SIG_01_BIT          (boss_sigs_t)(1 << 1)       /* 0x0002 */
+#define SIG_00_BIT          (boss_sigs_t)(1 << 0)       /* 0x0001 */
+
+//#define PRINTF(...)         printf_P(__VA_ARGS__)
+#define PRINTF(fmt, args...)      printf_P( PSTR(fmt), ##args )
+
+
+#endif  /* _BOSS_CONF_H_ */
---
+++ ex_CM3_KEIL_STM32/Boss_Conf.h
@@ -0,0 +1,156 @@
+#ifndef _BOSS_CONF_H_
+#define _BOSS_CONF_H_
+/*
+*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*
+*                                                                             *
+*                 RT-BOSS (Config File)         [ Cortex-M3 ]                 *
+*                                                                             *
+*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*=====*
+*/
+
+/*===========================================================================*/
+/*                            RT-BOSS 데이터형                               */
+/*---------------------------------------------------------------------------*/
+typedef unsigned char       boss_u08_t;       /* unsigned  8bit 데이터형 */
+typedef unsigned short int  boss_u16_t;       /* unsigned 16bit 데이터형 */
+typedef unsigned int        boss_u32_t;       /* unsigned 32bit 데이터형 */
+typedef unsigned __int64    boss_u64_t;       /* unsigned 64bit 데이터형 */
+
+//typedef boss_u08_t          boss_byte_t;      /* Byte                 */
+typedef boss_u32_t          boss_reg_t;       /* MCU 레지스터 크기      */
+typedef boss_u32_t          boss_uptr_t;      /* unsigned 포인터 크기   */
+typedef boss_u32_t          boss_stk_t;       /* 스택                   */
+
+typedef boss_u32_t          boss_sigs_t;      /* 시그널                 */
+typedef boss_u32_t          boss_tmr_ms_t;    /* 타이머 카운트(ms)      */
+
+typedef boss_u64_t          boss_mem_align_t; /* 메모리 정렬            */
+
+/*===========================================================================*/
+/*   IRQ (Interrupt request) / ISR (Interrupt Service Routine)               */
+/*---------------------------------------------------------------------------*/
+#define BOSS_IRQ_DISABLE()    do { \
+                                boss_reg_t _irq_storage_ = __get_PRIMASK(); \
+                                __disable_irq()
+                                    
+#define BOSS_IRQ_RESTORE()      __set_PRIMASK(_irq_storage_); \
+                              } while(0)
+
+/*----------------------------------------------------------------------*/
+#define BOSS_IRQ_LOCK_SR( _sr_ )  \
+                    do { _sr_ = __get_PRIMASK(); __disable_irq(); } while(0)
+            
+#define BOSS_IRQ_FREE_SR( _sr_ )      do { __set_PRIMASK(_sr_); } while(0)
+
+/*----------------------------------------------------------------------*/
+#define _BOSS_IRQ_()        __get_PRIMASK() /* 0 = Enable / !0 = Disable */
+
+#define _BOSS_ISR_()        __get_IPSR()    /* !0 = ISR Active  */
+
+/*----------------------------------------------------------------------*/
+#define _BOSS_ISR_BEGIN()      do { _Boss_sched_lock()
+#define _BOSS_ISR_FINIS()      _Boss_sched_free(); } while(0)
+
+
+/*===========================================================================*/
+/*                           RT-BOSS 사용자 설정                             */
+/*---------------------------------------------------------------------------*/
+//#define _BOSS_TCB_EXT_                /* TCB 확장(extend) */
+#define _BOSS_MEM_INFO_                 /* 메모리 디버거 정보 */
+
+#define _BOSS_TICK_MS_          1       /* Tick (ms)  */
+#define _IDLE_STACK_BYTES       128     /* Bytes      */
+#define _BOSS_MEM_POOL_SIZE     1024    /* Bytes      */
+
+
+/*===========================================================================*/
+/*                                    ASSERT                                 */
+/*---------------------------------------------------------------------------*/
+#define BOSS_ASSERT(expr) do { if(!(expr)) _assert(__MODULE__,__LINE__); } while(0)
+void _assert(const unsigned char *file, unsigned int line);
+
+
+/*===========================================================================*/
+/*                             태스크 우선순위                               */
+/*---------------------------------------------------------------------------*/
+typedef enum {
+  PRIO_BOSS_KERNEL = 0,   /* 상위 우선순위 */
+  
+  AA_PRIO_1,              /* 1. TOP */
+  BB_PRIO_2,              /* 2 */
+  
+  PRIO_BOSS_IDLE = 255,   /* 하위 우선순위 */
+} boss_prio_t;
+
+
+/*===========================================================================*/
+/*                           메시지 큐 명령어                                */
+/*---------------------------------------------------------------------------*/
+typedef enum {
+  M_CMD_EMPTY = 0,
+
+  M_CMD_1,
+  M_CMD_2,
+  /* ... */
+  
+  M_CMD_MAX = 255,
+} msg_cmd_t;
+
+
+/*===========================================================================*/
+/*                               INCLUDE FILE                                */
+/*---------------------------------------------------------------------------*/
+#include <stdio.h>  // "printf 사용을 위해"
+#include "stm32f10x.h"
+
+#include "Boss.h"
+#include "Boss_Tmr.h"
+#include "Boss_Mem.h"
+#include "Boss_Q_Msg.h"
+#include "Boss_Q_MBox.h"
+#include "Boss_Sem.h"
+
+
+/*===========================================================================*/
+/*                     USER DEFINE & FUNCTION PROTOTYPES                     */
+/*---------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------*/
+/*                       시그널 비트 정의                          */
+
+/* SIG_31_BIT ~ SIG_28_BIT (상위 4비트는 RT-BOSS에서 사용함)      */
+#define SIG_27_BIT    (boss_sigs_t)(1 << 27)    /* 0x 0800 0000 */
+#define SIG_26_BIT    (boss_sigs_t)(1 << 26)    /* 0x 0400 0000 */
+#define SIG_25_BIT    (boss_sigs_t)(1 << 25)    /* 0x 0200 0000 */
+#define SIG_24_BIT    (boss_sigs_t)(1 << 24)    /* 0x 0100 0000 */
+
+#define SIG_23_BIT    (boss_sigs_t)(1 << 23)    /* 0x 0080 0000 */
+#define SIG_22_BIT    (boss_sigs_t)(1 << 22)    /* 0x 0040 0000 */
+#define SIG_21_BIT    (boss_sigs_t)(1 << 21)    /* 0x 0020 0000 */
+#define SIG_20_BIT    (boss_sigs_t)(1 << 20)    /* 0x 0010 0000 */
+#define SIG_19_BIT    (boss_sigs_t)(1 << 19)    /* 0x 0008 0000 */
+#define SIG_18_BIT    (boss_sigs_t)(1 << 18)    /* 0x 0004 0000 */
+#define SIG_17_BIT    (boss_sigs_t)(1 << 17)    /* 0x 0002 0000 */
+#define SIG_16_BIT    (boss_sigs_t)(1 << 16)    /* 0x 0001 0000 */
+
+#define SIG_15_BIT    (boss_sigs_t)(1 << 15)    /* 0x 0000 8000 */
+#define SIG_14_BIT    (boss_sigs_t)(1 << 14)    /* 0x 0000 4000 */
+#define SIG_13_BIT    (boss_sigs_t)(1 << 13)    /* 0x 0000 2000 */
+#define SIG_12_BIT    (boss_sigs_t)(1 << 12)    /* 0x 0000 1000 */
+#define SIG_11_BIT    (boss_sigs_t)(1 << 11)    /* 0x 0000 0800 */
+#define SIG_10_BIT    (boss_sigs_t)(1 << 10)    /* 0x 0000 0400 */
+#define SIG_09_BIT    (boss_sigs_t)(1 << 9)     /* 0x 0000 0200 */
+#define SIG_08_BIT    (boss_sigs_t)(1 << 8)     /* 0x 0000 0100 */
+
+#define SIG_07_BIT    (boss_sigs_t)(1 << 7)     /* 0x 0000 0080 */
+#define SIG_06_BIT    (boss_sigs_t)(1 << 6)     /* 0x 0000 0040 */
+#define SIG_05_BIT    (boss_sigs_t)(1 << 5)     /* 0x 0000 0020 */
+#define SIG_04_BIT    (boss_sigs_t)(1 << 4)     /* 0x 0000 0010 */
+#define SIG_03_BIT    (boss_sigs_t)(1 << 3)     /* 0x 0000 0008 */
+#define SIG_02_BIT    (boss_sigs_t)(1 << 2)     /* 0x 0000 0004 */
+#define SIG_01_BIT    (boss_sigs_t)(1 << 1)     /* 0x 0000 0002 */
+#define SIG_00_BIT    (boss_sigs_t)(1 << 0)     /* 0x 0000 0001 */
+
+
+#define PRINTF(...)   printf(__VA_ARGS__)
+
+#endif  /* _BOSS_CONF_H_ */
