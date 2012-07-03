fc043558e653abc721262dd3b63956ce981d70bd
rtboss <rtboss@815b79aa-b098-0410-98f2-e32a66b021fa>
2012-07-03 06:20:18 +0000

    타이머 이중링크 구조로 변경. 카이머 콜백 기능 제거.
    git-svn-id: https://dev.naver.com/svn/rt-boss/trunk@63 815b79aa-b098-0410-98f2-e32a66b021fa

--- Boss/Boss_Tmr.c
+++ Boss/Boss_Tmr.c
@@ -30,73 +30,47 @@
 ---------------------------------------------------------------------------*/
 void _Boss_timer_tick(boss_tmr_ms_t tick_ms)
 {
-  boss_tmr_t  *p_prev;
   boss_tmr_t  *p_tmr;
-  boss_tmr_t  *p_done_list = _BOSS_NULL;
   
   BOSS_IRQ_DISABLE();
-  p_prev  = _BOSS_NULL;
-  p_tmr   = _boss_timer_list;
+  p_tmr = _boss_timer_list;
   
   while(p_tmr != _BOSS_NULL)
-  {    
+  {
     if(tick_ms < p_tmr->tmr_ms)
     {
       p_tmr->tmr_ms = p_tmr->tmr_ms - tick_ms;
-      
-      p_prev  = p_tmr;
-      p_tmr   = p_tmr->next;
+      p_tmr = p_tmr->next;
     }
     else  /* 타이머 완료 */
     {
-      boss_tmr_t  *p_done;
+      Boss_send(p_tmr->p_tcb, p_tmr->sig);      /* 시그널 처리 */
       
-      if(p_prev == _BOSS_NULL) {
-        _boss_timer_list = p_tmr->next;
-      } else {
-        p_prev->next = p_tmr->next;
+      if(p_tmr->rpt_ms != 0)                    /* 반복 타이머 */
+      {
+        p_tmr->tmr_ms = p_tmr->rpt_ms;
+        p_tmr = p_tmr->next;
       }
-      
-      p_done  = p_tmr;
-      p_tmr   = p_tmr->next;
-      
-      p_done->next = p_done_list;
-      p_done_list = p_done;
+      else                                      /* 타이머 제거 */
+      {
+        boss_tmr_t  *p_free = p_tmr;
+        p_tmr = p_tmr->next;
+        
+        if(p_free->prev == _BOSS_NULL) {
+          _boss_timer_list = p_free->next;
+        } else {
+          p_free->prev->next = p_free->next;
+        }
+        
+        if(p_free->next != _BOSS_NULL) {
+          p_free->next->prev = p_free->prev;
+        }
+        
+        Boss_mfree(p_free);                   /* 메모리 해제 */
+      }
     }
   }
   BOSS_IRQ_RESTORE();
-
-  while(p_done_list != _BOSS_NULL)        /* 완료 타이머 처리 */
-  {
-    boss_tmr_cb_t callback;
-    boss_tmr_t    *p_done;
-    
-    p_done = p_done_list;
-    p_done_list = p_done->next;
-    
-    if(p_done->sig != 0) {                /* 시그널 처리 */
-      Boss_send(p_done->p_tcb, p_done->sig);
-    }
-
-    callback = p_done->callback;
-
-    if(p_done->rpt_ms != 0)                /* 반복 타이머 재등록 */
-    {
-      p_done->tmr_ms = p_done->rpt_ms;
-      BOSS_IRQ_DISABLE();
-      p_done->next = _boss_timer_list;
-      _boss_timer_list = p_done;
-      BOSS_IRQ_RESTORE();
-    }
-    else
-    {
-      Boss_mfree(p_done);
-    }
-
-    if(callback != _BOSS_NULL) {        /* 콜백 함수 실행 */
-      callback();
-    }
-  }
 }
 
 
@@ -104,8 +78,8 @@ void _Boss_timer_tick(boss_tmr_ms_t tick_ms)
 /*===========================================================================
     B O S S _ T M R _ S T A R T
 ---------------------------------------------------------------------------*/
-boss_tmr_t *Boss_tmr_start( boss_tcb_t *p_tcb, boss_sigs_t sig,
-            boss_tmr_ms_t tmr_ms, boss_tmr_ms_t rpt_ms, boss_tmr_cb_t callback )
+void Boss_tmr_start( boss_tcb_t *p_tcb, boss_sigs_t sig,
+                                  boss_tmr_ms_t tmr_ms, boss_tmr_ms_t rpt_ms )
 {
   boss_tmr_t *p_tmr;
 
@@ -119,96 +93,59 @@ boss_tmr_t *Boss_tmr_start( boss_tcb_t *p_tcb, boss_sigs_t sig,
   p_tmr->tmr_ms = tmr_ms;
   p_tmr->rpt_ms = rpt_ms;
 
-  p_tmr->callback = callback;
-
-  BOSS_IRQ_DISABLE();
-  p_tmr->next = _boss_timer_list;         /* 타이머 추가 */
+  p_tmr->prev   = _BOSS_NULL;
+  p_tmr->next   = _BOSS_NULL;
+  
+  BOSS_IRQ_DISABLE();         /* 타이머 추가 */
+  if(_boss_timer_list != _BOSS_NULL)
+  {
+    BOSS_ASSERT(_boss_timer_list->prev == _BOSS_NULL);
+    
+    _boss_timer_list->prev = p_tmr;
+    p_tmr->next = _boss_timer_list;
+  }
   _boss_timer_list = p_tmr;
   
   Boss_sigs_clear(p_tcb, sig);
   BOSS_IRQ_RESTORE();
-
-  return p_tmr;
-}
-
-
-/*===========================================================================
-    B O S S _ T M R _ S T O P
----------------------------------------------------------------------------*/
-boss_reg_t Boss_tmr_stop(boss_tmr_t *p_tmr)
-{
-  boss_tmr_t *p_prev;
-  boss_tmr_t *p_find;
-
-  BOSS_IRQ_DISABLE();
-  p_prev = _BOSS_NULL;
-  p_find = _boss_timer_list;
-  while( (p_find != _BOSS_NULL) && (p_find != p_tmr) )
-  {
-    p_prev = p_find;
-    p_find = p_find->next;
-  }
-
-  if(p_find != _BOSS_NULL)        /* 타이머 제거 */
-  {
-    if(p_prev == _BOSS_NULL) {
-      _boss_timer_list = p_find->next;
-    } else {
-      p_prev->next = p_find->next;
-    }
-  }
-  BOSS_IRQ_RESTORE();
-  
-  if(p_find != _BOSS_NULL) {
-    Boss_mfree(p_find);
-    return _BOSS_TRUE;
-  }
-
-  return _BOSS_FALSE;
 }
 
 
 /*===========================================================================
     B O S S _ T M R _ C A N C E L
 ---------------------------------------------------------------------------*/
-boss_reg_t Boss_tmr_cancel(boss_tcb_t *p_tcb, boss_sigs_t sig,
-                                                        boss_tmr_cb_t callback)
+void Boss_tmr_cancel(boss_tcb_t *p_tcb, boss_sigs_t sig )
 {
-  boss_tmr_t *p_prev;
   boss_tmr_t *p_find;
 
   BOSS_IRQ_DISABLE();
-  p_prev = _BOSS_NULL;
   p_find = _boss_timer_list;
   
   while( p_find != _BOSS_NULL )
   {
-    if( (p_find->p_tcb == p_tcb) && (p_find->sig == sig) 
-                    && (p_find->callback == callback) )
+    if( (p_find->p_tcb == p_tcb) && (p_find->sig == sig) )  /* 타이머 제거 */
     {
-      break;
+      boss_tmr_t  *p_free = p_find;
+      p_find = p_find->next;
+      
+      if(p_free->prev == _BOSS_NULL) {
+        _boss_timer_list = p_free->next;
+      } else {
+        p_free->prev->next = p_free->next;
+      }
+      
+      if(p_free->next != _BOSS_NULL) {
+        p_free->next->prev = p_free->prev;
+      }
+      
+      Boss_mfree(p_free);                                     /* 메모리 해제 */
     }
-    
-    p_prev = p_find;
-    p_find = p_find->next;
-  }
-  
-  if(p_find != _BOSS_NULL)        /* 타이머 제거 */
-  {
-    if(p_prev == _BOSS_NULL) {
-      _boss_timer_list = p_find->next;
-    } else {
-      p_prev->next = p_find->next;
+    else
+    {
+      p_find = p_find->next;
     }
-  }
+  }  
   BOSS_IRQ_RESTORE();
-
-  if(p_find != _BOSS_NULL) {
-    Boss_mfree(p_find);
-    return _BOSS_TRUE;
-  }
-
-  return _BOSS_FALSE;
 }
 
 
@@ -217,7 +154,7 @@ boss_reg_t Boss_tmr_cancel(boss_tcb_t *p_tcb, boss_sigs_t sig,
 ---------------------------------------------------------------------------*/
 void Boss_sleep(boss_tmr_ms_t wait_ms)
 {
-  Boss_tmr_start(Boss_self(), BOSS_SIG_SLEEP, wait_ms, _BOSS_FALSE, _BOSS_NULL);
+  Boss_tmr_start(Boss_self(), BOSS_SIG_SLEEP, wait_ms, _BOSS_FALSE);
 
   Boss_wait(BOSS_SIG_SLEEP);
 }
@@ -230,12 +167,12 @@ boss_sigs_t Boss_wait_sleep(boss_sigs_t wait_sigs,  boss_tmr_ms_t wait_ms)
 {
   boss_sigs_t recv_sigs;
   
-  Boss_tmr_start(Boss_self(), BOSS_SIG_SLEEP, wait_ms, _BOSS_FALSE, _BOSS_NULL);
+  Boss_tmr_start(Boss_self(), BOSS_SIG_SLEEP, wait_ms, _BOSS_FALSE);
 
   recv_sigs = Boss_wait(wait_sigs | BOSS_SIG_SLEEP);
 
   if( (recv_sigs & BOSS_SIG_SLEEP) == 0 ) {
-    Boss_tmr_cancel(Boss_self(), BOSS_SIG_SLEEP, _BOSS_NULL);
+    Boss_tmr_cancel(Boss_self(), BOSS_SIG_SLEEP);
   }
   
   return recv_sigs;
--- Boss/Boss_Tmr.h
+++ Boss/Boss_Tmr.h
@@ -18,9 +18,8 @@
 /*---------------------------------------------------------------------------*/
 #define BOSS_TMR_INFINITE               (~(boss_tmr_ms_t)0)
 
-typedef void (*boss_tmr_cb_t)(void);
-
 typedef struct boss_tmr_struct {
+  struct boss_tmr_struct  *prev;
   struct boss_tmr_struct  *next;
   
   boss_tcb_t        *p_tcb;         /* TCB                         */
@@ -28,8 +27,6 @@ typedef struct boss_tmr_struct {
   
   boss_tmr_ms_t     tmr_ms;         /* Timer Countdown Millisecond */
   boss_tmr_ms_t     rpt_ms;         /* Repeat Count Millisecond    */
-  
-  boss_tmr_cb_t     callback;       /* Callback Function           */
 } boss_tmr_t;
 
 
@@ -38,12 +35,10 @@ typedef struct boss_tmr_struct {
 /*---------------------------------------------------------------------------*/
 void _Boss_timer_tick(boss_tmr_ms_t tick_ms);
 
-boss_tmr_t *Boss_tmr_start( boss_tcb_t *p_tcb, boss_sigs_t sig,
-            boss_tmr_ms_t tmr_ms, boss_tmr_ms_t rpt_ms, boss_tmr_cb_t callback );
+void Boss_tmr_start( boss_tcb_t *p_tcb, boss_sigs_t sig,
+                                    boss_tmr_ms_t tmr_ms, boss_tmr_ms_t rpt_ms);
 
-boss_reg_t Boss_tmr_stop(boss_tmr_t *p_tmr);
-boss_reg_t Boss_tmr_cancel(boss_tcb_t *p_tcb, boss_sigs_t sig,
-                                                        boss_tmr_cb_t callback);
+void Boss_tmr_cancel(boss_tcb_t *p_tcb, boss_sigs_t sig);
 
 void Boss_sleep(boss_tmr_ms_t wait_ms);
 boss_sigs_t Boss_wait_sleep(boss_sigs_t wait_sigs,  boss_tmr_ms_t wait_ms);
